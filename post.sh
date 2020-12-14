616b47c4e9641c9c3a1be17e0926ee7f2fb96d93
An-Yi-Huang <anyi.huang@ucdconnect.ie>
2020-12-14 18:09:51 +0000

    2nd commit

--- post.sh
+++ post.sh
@@ -1,31 +1,34 @@
 #! /bin/bash
 
-receiver=$1
-sender=$2
-message=$3
+receiver="$1"
+sender="$2"
+message="$3"
+
+echo $receiver
+echo $sender
 
 if [ $# -ne 3 ]; then
   echo "Error: parameters problem"
   exit 1
 fi
 
-if ! [ -d $receiver ]; then
+if ! [ -d "$receiver" ]; then
   echo "Error: Receiver does not exist"
   exit 2
 fi
 
-if ! [ -d $sender ]; then
+if ! [ -d "$sender" ]; then
   echo "Error: Sender does not exist"
   exit 2
 fi
 
-./P.sh $receiver/friends
-if [ -z `grep $sender $receiver/friends` ]; then
+./P.sh "$receiver"/friends
+if grep -q "$sender" "$receiver"/friends ; then
   echo "Error: Sender is not a friend of receiver"
-  ./V.sh $receiver/friends
+  ./V.sh "$receiver"/friends
   exit 3
 fi
 
-echo "$sender: $message" >> $receiver/wall
-./V.sh $receiver/friends
+echo "$sender: $message" >> "$receiver"/wall
+./V.sh "$receiver"/friends
 echo "Ok: Message posted to wall"
--- show.sh
+++ show.sh
@@ -7,11 +7,11 @@ if [ $# -ne 1 ]; then
   exit 1
 fi
 
-if ! [ -d $user ]; then
+if ! [ -d "$user" ]; then
   echo "Error: user does not exist"
   exit 2
 fi
 
 echo "wallStart"
-cat $user/wall
+cat "$user"/wall
 echo "wallEnd"
